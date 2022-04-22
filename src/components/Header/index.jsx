9cbcae97c5955bb56ab0c8a3bee6fce59d0bf062
gdantasc <gabrieldcavalcante00@gmail.com>
2022-04-22 20:42:23 -0300

    feat: add icons with reference

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HeaderComponent, Path, Media } from "./style";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -13,8 +14,20 @@ const Header = () => {
           <a>Contato</a>
         </Path>
         <Media>
-          <a>Github</a>
-          <a>Linkedin</a>
+          <a
+            href="https://github.com/gdantasc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gabriel-d-cavalcante/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin size={30} />
+          </a>
         </Media>
       </nav>
     </HeaderComponent>
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -6,12 +6,13 @@ export const HeaderComponent = styled.header`
   border-bottom: 2px solid var(--blue-light);
   display:flex ;
   color: #fff;
+  font-weight: bold;
 
-    margin: 0 auto;
-    padding: 0 2rem;
+  margin: 0 auto;
+  padding: 0 2rem;
 
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
     nav {
         height: 5rem;
@@ -61,17 +62,21 @@ export const Path = styled.div`
 
 export const Media = styled.div`
   display: flex;
-  display: inline-block;
+  align-items: center;
+  justify-content: center;
+
   padding: 0 0.5rem;
   height: 5rem;
-  line-height: 5rem;
   transition: color 0.2s;
   cursor: pointer;
 
   a {
-    margin: 0 10px;
+    margin: 0 15px;
+    text-decoration: none;
+    color: #fff;
+
     :hover {
-      filter: brightness(0.8);
+      filter: brightness(0.5);
     }
   }
 `;
