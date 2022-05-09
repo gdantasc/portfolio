ba27f2afb0abb712ac6380b1df4953eb5462f736
gdantasc <gabrieldcavalcante00@gmail.com>
2022-05-09 18:57:43 -0300

    feat: Project Organism

---
+++ src/components/Organisms/Projects/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ProjectContainer } from "./style";
+
+const Project = () => {
+  return (
+    <ProjectContainer id="projects">
+      <div>
+        <h1>Projetos</h1>
+      </div>
+    </ProjectContainer>
+  );
+};
+
+export default Project;
