63a60a6211ae433f16feb74513b6fafa60a8e5eb
gdantasc <gabrieldcavalcante00@gmail.com>
2022-05-09 16:02:02 -0300

    feat: About Organism

---
+++ src/components/Organisms/About/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AboutContainer } from "./style";
+
+const About = () => {
+  return (
+    <AboutContainer id="about">
+      <div>
+        <h1>Sobre</h1>
+      </div>
+    </AboutContainer>
+  );
+};
+
+export default About;
