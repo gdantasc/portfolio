0ea2ae27c92f71ad308b9699c22f35e9f3ff2848
gdantasc <gabrieldcavalcante00@gmail.com>
2022-05-09 18:57:26 -0300

    feat: Contact Organism

---
+++ src/components/Organisms/Contact/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ContactContainer } from "./style";
+
+const Contact = () => {
+  return (
+    <ContactContainer id="contact">
+      <div>
+        <h1>Contato</h1>
+      </div>
+    </ContactContainer>
+  );
+};
+
+export default Contact;
