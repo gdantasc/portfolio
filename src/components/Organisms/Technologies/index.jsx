66e533f806f49275b97bde5bda4f886aaebb8435
gdantasc <gabrieldcavalcante00@gmail.com>
2022-05-09 18:58:04 -0300

    feat: Tecnologies Organism

---
+++ src/components/Organisms/Technologies/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TechnologiesContainer } from "./style";
+
+const Technologies = () => {
+  return (
+    <TechnologiesContainer id="technologies">
+      <div>
+        <h1>Tecnologias</h1>
+      </div>
+    </TechnologiesContainer>
+  );
+};
+
+export default Technologies;
---
+++ src/components/Organisms/Technologies/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const TechnologiesContainer = styled.div`
+  height: calc(100vh - 5rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
