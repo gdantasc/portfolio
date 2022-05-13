a9fd81e925065911e2b8b6b2290ac3c0d374574f
gdantasc <gabrieldcavalcante00@gmail.com>
2022-05-12 22:56:39 -0300

    refactor: technologies screen

--- src/components/Organisms/Technologies/index.jsx
+++ src/components/Organisms/Technologies/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { TechnologiesContainer } from "./style";
-
+import Card from "../../Atoms/Card";
 const Technologies = () => {
   return (
     <TechnologiesContainer id="technologies">
       <div>
         <h1>Tecnologias</h1>
+        <Card name="HTML" />
       </div>
     </TechnologiesContainer>
   );
