79f61c0371636d288e0ba27aac9e8e3b0ef5b595
gdantasc <gabrieldcavalcante00@gmail.com>
2022-05-17 23:06:52 -0300

    refactor:  index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +9,25 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const toggle = document.querySelector(".hover-show");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+});
+
+let Icons = document.querySelectorAll(".navigation .icon");
+
+Icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    changeactive();
+
+    icon.classList.add("active-nav");
+  });
+});
+
+function changeactive() {
+  Icons.forEach((icon) => {
+    icon.classList.remove("active-nav");
+  });
+}
